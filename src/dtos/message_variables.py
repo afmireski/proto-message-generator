8021d7b77d56c9bbe132f5f77ff1fad19aa0151a
afmireski <afmireski@outlook.com>
2023-01-21 09:30:40 -0300

    #9 - Change 'convert_type' to reconize 'keyof typeof'

--- src/dtos/message_variables.py
+++ src/dtos/message_variables.py
@@ -1,13 +1,17 @@
 class MessageVariable:
     def __init__(self, var_type: str, name: str, index: int, is_array: bool = False):
-        self.var_type = self.convert_type(var_type)
+        self.var_type = self.__convert_type(var_type)
         self.name = name
         self.index = index
         self.is_array = is_array
 
     @staticmethod
-    def convert_type(var_type: str) -> str:
-        if var_type == 'string' or var_type == 'Date' or var_type.lower().__contains__('enum'):
+    def __should_be_string(var_type: str) -> bool:
+        return var_type == 'string' or var_type == 'Date' or var_type.lower().__contains__('enum') \
+            or var_type.lower().__contains__('keyof typeof')
+
+    def __convert_type(self, var_type: str) -> str:
+        if self.__should_be_string(var_type):
             return 'string'
         elif var_type == 'number':
             return 'int32'
